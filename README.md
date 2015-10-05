730c56c5b2a4b793ff0695322242cdd442765028
Nico de Wet <nico@nicodewet.com>
2015-10-06 12:22:34 +1300

    oauth/authorize and oauth/token end-points up and running

--- README.md
+++ README.md
@@ -15,3 +15,21 @@ Spring Security OAuth2
   <artifactId>spring-security-oauth2</artifactId>
   <version>2.0.7.RELEASE</version>
 </dependency>
+
+Up and Running
+--------------
+
+$ mvn spring-boot:run 
+
+Visit: http://localhost:9999/uaa/oauth/authorize?response_type=code&client_id=acme&redirect_uri=http://example.com
+
+Server is using Spring Boot default security settings, so the above will be protected using HTTP Basic authentication. With the above you are visiting the *authorization endpoint* and initiating an authorizaton code token grant.
+
+Redirected to: http://example.com/?code=OivfyQ
+
+We now exchange the authorization code for an access token using the *token endpoint*.
+
+$ curl acme:acmesecret@localhost:9999/uaa/oauth/token -d grant_type=authorization_code -d client_id=acme -d redirect_uri=http://example.com -d code=OivfyQ
+ 
+{"access_token":"ce9222cb-6297-43bd-bcc0-d03281779afc","token_type":"bearer","refresh_token":"9291ad4d-b69e-4f28-b346-ed6709d59bf4","expires_in":43199,"scope":"openid"}
+
--- src/main/java/demo/AuthserverApplication.java
+++ src/main/java/demo/AuthserverApplication.java
@@ -1,12 +1,41 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @SpringBootApplication
 public class AuthserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthserverApplication.class, args);
+    }
+    
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+      @Autowired
+      private AuthenticationManager authenticationManager;
+      
+      @Override
+      public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+      }
+
+      @Override
+      public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+            .withClient("acme")
+            .secret("acmesecret")
+            .authorizedGrantTypes("authorization_code", "refresh_token",
+                "password").scopes("openid");
+      }
     }
 }
